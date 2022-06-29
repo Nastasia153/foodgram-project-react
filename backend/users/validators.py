b1afd68b1134c6510876078a5e5d6fa42464bab6
Nastasia Dem <oo.cl@ya.ru>
2022-06-29 11:46:08 +0400

    before deploy

--- backend/users/validators.py
+++
@@ -1,8 +0,0 @@
-from django.core.validators import RegexValidator
-
-
-def username_validator():
-    return RegexValidator(
-        regex=r'^[\w.@+-]+$',
-        message='Допускаются буквы, цифры и знаки _ @ / + - .'
-    )
